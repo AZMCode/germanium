27214430c08108dd0c5765f072a34475244555b0
Adriano Zambrana Marchetti <adrianozambrana@protonmail.com>
2022-07-23 22:10:21 -0400

    Add support for transparent backgrounds
    Solves #35

--- util.go
+++ util.go
@@ -11,8 +11,16 @@ func ParseHexColor(s string) (color.RGBA, error) {
 
 	var err error
 	switch len(s) {
+	case 9:
+		_, err = fmt.Sscanf(s, "#%02x%02x%02x%02x", &c.R, &c.G, &c.B, &c.A)
 	case 7:
 		_, err = fmt.Sscanf(s, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+	case 5:
+		_, err = _, err = fmt.Sscanf(s, "#%1x%1x%1x%1x", &c.R, &c.G, &c.B, &c.A)
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+		c.A *= 17
 	case 4:
 		_, err = fmt.Sscanf(s, "#%1x%1x%1x", &c.R, &c.G, &c.B)
 		c.R *= 17
